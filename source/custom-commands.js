a01cf6da3cf0f9d0e65bbe1e3903309155451d69
BeforeMath <tyrekelewis@yahoo.ca>
2014-06-02 12:01:07 -0400

    Update custom-commands.js

--- source/custom-commands.js
+++ source/custom-commands.js
@@ -1818,7 +1818,6 @@ restart: function(target, room, user) {
        busy: 'away',
        afk: 'away',
        away: function(target, room, user, connection, cmd) {
-            if (!this.can('away')) return false;
             // unicode away message idea by Siiilver
             var t = 'Ⓐⓦⓐⓨ';
             var t2 = 'Away';
@@ -1878,7 +1877,6 @@ user.updateIdentity();
 },
 
 back: function(target, room, user, connection) {
-if (!this.can('away')) return false;
 
 if (user.isAway) {
 if (user.name === user.originalName) {
